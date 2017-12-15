375b654abf3ae3d22422c50d92d3f4f5bd07997c
cdjay <76025226@qq.com>
2017-12-15 17:34:01 +0800

    complete

--- ocr.py
+++ ocr.py
@@ -1,5 +1,6 @@
+import time
+start = time.clock()
 from aip import AipOcr
-
 """ 你的 APPID AK SK """
 APP_ID = '10532375'
 API_KEY = 'k4Lucb2SnVotnYfnBgkWskKs'
@@ -17,9 +18,11 @@ def gt(filename):
     result = client.basicAccurate(get_file_content(filename)) #高精度
     return result
 # 调用通用文字识别接口
-try:
-    for i in range(1,10):
-        filename=str(i)+'.jpg'
-        print(gt(filename))
-except:
-    print('not!')
+# try:
+#     for i in range(1,10):
+#         filename=str(i)+'.jpg'
+#         print(gt(filename))
+# except:
+#     print('not!')
+print(gt('new3.jpg'))
+print('耗时:',time.clock() - start,'秒')
